e7d3a7481acc430d05a13396b5697a5e7d72a688
Kyle Kroger <kyle.s.kroger@gmail.com>
2022-12-23 19:24:54 -0600

    add special menu and menu item component

--- components/Menuitem/MenuItem.jsx
+++ components/Menuitem/MenuItem.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 import styles from "./MenuItem.module.css";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+const MenuItem = ({ title, price, tags }) => {
+  return (
+    <div className={`${styles["app__menuitem"]}`}>
+      <div className={`${styles["app__menuitem-head"]}`}>
+        <div className={`${styles["app__menuitem-name"]}`}>
+          <p className={`p__cormorant`} style={{ color: "#DCCA87" }}>
+            {title}
+          </p>
+        </div>
+
+        <div className={`${styles["app__menuitem-dash"]}`} />
+
+        <div className={`${styles["app__menuitem-price"]}`}>
+          <p className={`p__cormorant`}>{price}</p>
+        </div>
+      </div>
+      <div className={`${styles["app__menuitem-sub"]}`}>
+        <p
+          className={`p__opensans`}
+          style={{ color: "#aaa", marginTop: "0.5rem" }}
+        >
+          {tags}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MenuItem;
--- container/Menu/SpecialMenu.jsx
+++ container/Menu/SpecialMenu.jsx
@@ -1,7 +1,72 @@
+import Image from "next/image";
 import React from "react";
 
+import { SubHeading, MenuItem } from "../../components";
+import { data } from "../../constants";
 import styles from "./SpecialMenu.module.css";
 
-const SpecialMenu = () => <div>SpecialMenu</div>;
+const SpecialMenu = () => (
+  <div
+    className={`${styles.app__specialMenu} flex__center section__padding`}
+    id="menu"
+  >
+    <div className={`${styles["app__specialMenu-title"]}`}>
+      <SubHeading title="Menu that fits your Palatte" />
+      <h1 className={`headtext__cormorant`}>Today's Special</h1>
+    </div>
+
+    <div className={`${styles["app__specialMenu-menu"]}`}>
+      <div className={`${styles["app__specialMenu-menu_wine"]} flex__center`}>
+        <p className={`${styles["app__specialMenu-menu_heading"]}`}>
+          Wine & Beer
+        </p>
+        <div className={`${styles["app__specialMenu-menu_items"]}`}>
+          {data.wines.map((wine, i) => (
+            <MenuItem
+              key={wine.title + i}
+              title={wine.title}
+              price={wine.price}
+              tags={wine.tags}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className={`${styles["app__specialMenu-menu_img"]}`}>
+        <Image
+          className={styles.menuImage}
+          src={"/images/menu.png"}
+          alt="menu"
+          width={0}
+          height={0}
+          sizes="100vw"
+        />
+      </div>
+
+      <div
+        className={`${styles["app__specialMenu-menu_cocktails"]} flex__center`}
+      >
+        <p className={`${styles["app__specialMenu-menu_heading"]}`}>
+          Cocktails
+        </p>
+        <div className={`${styles["app__specialMenu-menu_items"]}`}>
+          {data.cocktails.map((cocktail, i) => (
+            <MenuItem
+              key={cocktail.title + i}
+              title={cocktail.title}
+              price={cocktail.price}
+              tags={cocktail.tags}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+    <div style={{ marginTop: "15px" }}>
+      <button type="button" className={`custom__button`}>
+        View More
+      </button>
+    </div>
+  </div>
+);
 
 export default SpecialMenu;
