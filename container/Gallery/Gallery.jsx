1d26fda441e9b1a0512ac634e53bd5cedd5a8b26
Kyle Kroger <kyle.s.kroger@gmail.com>
2022-12-29 19:09:59 -0600

    add laurels

--- container/Gallery/Gallery.jsx
+++ container/Gallery/Gallery.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import Image from "next/image";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 import styles from "./Gallery.module.css";
 
-const Gallery = () => <div>Gallery</div>;
+const imageArr = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
+
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === "left") {
+      current.scrollleft -= 300;
+    } else {
+      current.scrollleft += 300;
+    }
+  };
+
+  return (
+    <div className={`${styles["app__gallery"]} flex__center section__padding`}>
+      <div className={`${styles["app__gallery-content"]}`}>
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: "#aaa", marginTop: "2rem" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci
+          ducimus dolorum, quos impedit quo laboriosam voluptate repudiandae
+          praesentium.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div className={`${styles["app__gallery-images"]}`}>
+        <div
+          className={`${styles["app__gallery-images_container"]}`}
+          ref={scrollRef}
+        >
+          {imageArr.map((image, index) => (
+            <div
+              className={`${styles["app__gallery-images_card"]} flex__center`}
+              key={`gallery_image-${index + 1}`}
+            >
+              <Image
+                className={styles.galleryImg}
+                src={image}
+                alt="gallery_image"
+                width="0"
+                height="0"
+                sizes="100vw"
+              />
+              <BsInstagram className={styles.instaIcon} />
+            </div>
+          ))}
+        </div>
+        <div className={`${styles["app__gallery-images_arrows"]}`}>
+          <BsArrowLeftShort
+            className={styles.arrowIcon}
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className={styles.arrowIcon}
+            onClick={() => scroll("right")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
--- container/Laurels/Laurels.jsx
+++ container/Laurels/Laurels.jsx
@@ -1,17 +1,54 @@
 import React from "react";
+import Image from "next/image";
 
 import { SubHeading } from "../../components";
-import { data } from "../../constants";
+import { data, images } from "../../constants";
 
 import styles from "./Laurels.module.css";
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
+  return (
+    <div className={`${styles["app__laurels_awards-card"]}`}>
+      <Image
+        className={styles.awardImg}
+        src={imgUrl}
+        alt="title"
+        width="0"
+        height="0"
+        sizes="100vw"
+      />
+      <div className={`${styles["app__laurels_awards-card_content"]}`}>
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+        <p className="p__cormorant">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
 const Laurels = () => (
   <div className={`app__bg app__wrapper section__padding`} id="awards">
-    <div className={`${styles["app__wrapper_info"]}`}>
+    <div className="app__wrapper_info">
       <SubHeading title="Awards & recognition" />
       <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className={`${styles["app__laurels_awards"]}`}>
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <Image
+        className={styles.laurelsImg}
+        src={images.laurels}
+        alt="laurels"
+        width="0"
+        height="0"
+        sizes="100vw"
+      />
+    </div>
   </div>
 );
 
